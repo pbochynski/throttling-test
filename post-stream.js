459931bb1f1ee2cde1cd6ecb441c6babf0600f07
Bochynski, Piotr <piotr.bochynski@hybris.de>
2016-12-01 09:12:34 +0100

    Heroku added

--- post-stream.js
+++ post-stream.js
@@ -4,10 +4,11 @@ var serviceUrl = "https://throttling-test.eu-central.cf.yaas.io";
 var hcpServiceUrl = "https://throttling-test.cfapps.us10.hana.ondemand.com";
 var bluemixServiceUrl = "https://throttling-test.eu-gb.mybluemix.net";
 var pivotalServiceUrl = "https://throttling-test.cfapps.io/";
+var herokuServiceUrl = "https://arcane-depths-87484.herokuapp.com";
 
 function importData() {
     var stream = new Generator(null, 200000);
-    stream.pipe(request.post(pivotalServiceUrl, {
+    stream.pipe(request.post(herokuServiceUrl, {
             headers: {'content-type': 'application/json'}
         }, function (err, response, body) {
             if (err) throw err;
