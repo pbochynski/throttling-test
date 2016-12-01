1ef52ba35157dc8d656947ce35869e433de5190b
Bochynski, Piotr <piotr.bochynski@hybris.de>
2016-12-01 09:14:55 +0100

    Default post on localhost

--- post-stream.js
+++ post-stream.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var Generator = require('./generator');
+var localUrl = "http://localhost:9090";
 var serviceUrl = "https://throttling-test.eu-central.cf.yaas.io";
 var hcpServiceUrl = "https://throttling-test.cfapps.us10.hana.ondemand.com";
 var bluemixServiceUrl = "https://throttling-test.eu-gb.mybluemix.net";
@@ -8,7 +9,7 @@ var herokuServiceUrl = "https://arcane-depths-87484.herokuapp.com";
 
 function importData() {
     var stream = new Generator(null, 200000);
-    stream.pipe(request.post(herokuServiceUrl, {
+    stream.pipe(request.post(localUrl, {
             headers: {'content-type': 'application/json'}
         }, function (err, response, body) {
             if (err) throw err;
